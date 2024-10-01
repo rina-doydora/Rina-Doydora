f5f229ee00269241a45e754f94d7015d7b0b5354
rina-doydora <doydorarina0@gmail.com>
2024-10-01 13:38:03 +0800

    Add files via upload

---
+++ Rina Doydora/index/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login-form</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+
+<main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">Firstname</label>
+            <input id="firstname" type="text" name="firstname" placeholder="firstname">
+
+            <label for="lastname">Lastname</label>
+            <input id="lastname" type="text" name="lastname" placeholder="Lastname">
+
+            <label for="gender">Gender ?</label>
+            <select id ="gender"name="gender">
+                <option value="none">None</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+
+            </select>
+
+            <button type="submit">Submit</button>
+        </form>  
+        
+    
+</body>
+</html>
